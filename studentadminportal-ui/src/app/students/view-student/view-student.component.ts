088c310987d0a92152c44f3ed1a5a12c40f13a0d
SumitShekhawatOctal <120454613+SumitShekhawatOctal@users.noreply.github.com>
2023-06-23 19:29:49 +0530

    update students details
    update students details

--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../student.service';
 import { ActivatedRoute } from '@angular/router';
 import { Student } from 'src/app/models/ui-models/student.model';
+import { GenderService } from 'src/app/Services/gender.service';
+import { Gender } from 'src/app/models/ui-models/gender.model';
 
 @Component({
   selector: 'app-view-student',
@@ -30,8 +32,11 @@ export class ViewStudentComponent implements OnInit {
       postalAddress:''
     },
   };
+  genderList: Gender[] = [];
   constructor(private readonly studentService: StudentService
-    ,private readonly route: ActivatedRoute) { }
+    ,private readonly route: ActivatedRoute
+    ,private readonly genderService: GenderService
+    ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
@@ -45,8 +50,28 @@ export class ViewStudentComponent implements OnInit {
               this.student = successResponse;
             }
           );
+          this.genderService.getGenderList()
+          .subscribe(
+            (successResponse)=>{
+              //console.log(successResponse);
+              this.genderList = successResponse;
+            });
         }
       });
+  }
+
+  onUpdate(): void{
+    //call student service to update student details
+    this.studentService.updateStudent(this.student.id,this.student)
+    .subscribe(
+      (successResponse)=>{
+        console.log(successResponse);
+        //show a notification
+      },
+      (error) => {
+        //log it
+        console.log(error);
+      })
   }
 
 }
